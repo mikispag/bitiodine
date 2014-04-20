2b034ae5474642683bcf9804b6742783abcb2a69
Michele Spagnuolo <michele@spagnuolo.me>
2014-04-20 19:44:04 +0200

    Use redisFree() correctly.

--- deploy/lemon/redis.h
+++ deploy/lemon/redis.h
@@ -6,38 +6,6 @@ void redisDisconnect(redisContext *ctx)
     redisFree(ctx);
 }
 
-void displayRedisReply(redisReply *reply)
-{
-
-    if (!reply) {
-        return;
-    }
-
-    switch (reply->type)
-    {
-    case REDIS_REPLY_STATUS:
-        printf("REDIS_REPLY_STATUS: %s\n", reply->str);
-        break;
-    case REDIS_REPLY_ERROR:
-        printf("REDIS_REPLY_ERROR: %s\n", reply->str);
-        break;
-    case REDIS_REPLY_INTEGER:
-        printf("REDIS_REPLY_INTEGER: %lld\n", reply->integer);
-        break;
-    case REDIS_REPLY_NIL:
-        printf("REDIS_REPLY_NIL\n");
-        break;
-    case REDIS_REPLY_STRING:
-        printf("REDIS_REPLY_STRING: %s\n", reply->str);
-        break;
-    case REDIS_REPLY_ARRAY:
-        printf("REDIS_REPLY_ARRAY\n");
-        break;
-    default:
-        break;
-    }
-}
-
 std::string getRedisString(redisReply *reply)
 {
     std::string redisString;
@@ -45,6 +13,7 @@ std::string getRedisString(redisReply *reply)
     if (reply && reply->type == REDIS_REPLY_STRING)
     {
         redisString = reply->str;
+        redisFree(reply);
     }
     else return "";
 
